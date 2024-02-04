9ad92c6cba44a5af388397bbbbf06c970b5dff96
Iván Raskovsky <raskovsky+git@gmail.com>
2024-02-04 00:57:59 -0300

    improve sines

--- lib/effects/Sines.h
+++ lib/effects/Sines.h
@@ -11,20 +11,25 @@ public:
 
     long step;
     bool paint() override {
-        step++;
+        step += mapf(gyroData.pitch, (float)-M_PI/2, (float)M_PI/2, 1.0f, 2.5f);
+        // 2 good options
+        // int length = mapf(gyroData.roll, (float)-M_PI/2, (float)M_PI/2, 1.0f, 5.0f);
+        int length = mapf(gyroData.roll, (float)-M_PI/2, (float)M_PI/2, 5.0f, 15.0f);
         auto& segmentsVector = segments.getSegments();
 
         for (int i = 0; i < segmentsVector.size(); i++) {
 
             auto& segment = segmentsVector[i];
-            uint8_t brightness = map(sin(mapf(((step + i) % 255), 0, 255, 0, 2 * M_PI)), -1, 1, 0, 255);
+            uint8_t brightness = map(sin(mapf(((int)(step + i * length) % 255), 0, 255, 0, 2 * M_PI)), -1, 1, 0, 255);
             FillPainter::paint(segment, CHSV(map(gyroData.yaw, (float)-M_PI, (float)M_PI, 0, 255), 255, brightness));
 
-            // for (int i = 0; i < segment.getSize(); ++i) {
-            //     segment.getLEDs()[i] = CRGB(
-            //         127 + 127 * sin(i + millis() / 1000.0),
-            //         127 + 127 * sin(i + millis() / 1000.0 + 2 * PI / 3),
-            //         127 + 127 * sin(i + millis() / 1000.0 + 4 * PI / 3)
+
+
+            // for (int j = 0; j < segment.getSize(); ++j) {
+            //     segment.getLEDs()[j] = CRGB(
+            //         127 * sin(j / 5 + millis() / 1000.0),
+            //         127 * sin(j / 5 + millis() / 1000.0 + 2 * PI / 3),
+            //         127 * sin(j / 5 + millis() / 1000.0 + 4 * PI / 3)
             //     );
             // }
         }
