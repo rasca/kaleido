850465a769ac3d4148e205e4181fe04170606855
Iván Raskovsky <raskovsky+git@gmail.com>
2024-02-04 20:06:09 -0300

    adjust choreographer

--- lib/effects/Choreographer.h
+++ lib/effects/Choreographer.h
@@ -22,10 +22,23 @@ class Choreographer
 
     public:
 
-    Choreographer(BaseKaleido<NUM_LEDS>& kaleido) : kaleido(kaleido), currentEffect(new Sines(kaleido.lines)) {}
+    std::vector<std::function<void(Choreographer*)>> effects;
+
+
+    Choreographer(BaseKaleido<NUM_LEDS>& kaleido) : kaleido(kaleido), currentEffect(new Sines(kaleido.lines))
+    {
+        effects.push_back(&Choreographer::sinesSlow);
+        effects.push_back(&Choreographer::sinesFast);
+        effects.push_back(&Choreographer::start);
+        effects.push_back(&Choreographer::outwards);
+        effects.push_back(&Choreographer::outwardsFacets);
+        effects.push_back(&Choreographer::stars);
+        effects.push_back(&Choreographer::hopeFacets);
+        effects.push_back(&Choreographer::hopeLines);
+    }
 
     long effectStart = 0;
-    long effectDuration = 10000;  // in millis
+    long effectDuration = 30000;  // in millis
 
     void paint()
     {
@@ -40,43 +53,61 @@ class Choreographer
 
     void nextEffect()
     {
-        Serial.println("nextEffect");
+        FastLED.clear();
         delete currentEffect;
 
-        int i = random(0, 5);
-        switch (i)
-        {
-        case 0: {
-            Sines *sinesSlow = new Sines(kaleido.lines);
-            sinesSlow->minLength = 1.0f;
-            sinesSlow->maxLength = 5.0f;
-            sinesSlow->minStepVelocity = 1.0f;
-            sinesSlow->maxStepVelocity = 2.5f;
-            currentEffect = sinesSlow;
-            break;
-        }
-        case 1: {
-            Sines *sinesFast = new Sines(kaleido.lines);
-            sinesFast->minLength = 5.0f;
-            sinesFast->maxLength = 15.0f;
-            sinesFast->minStepVelocity = 1.0f;
-            sinesFast->maxStepVelocity = 2.5f;
-            currentEffect = sinesFast;
-            break;
-        }
-        case 2: {
-            currentEffect = new Start(kaleido.facets, kaleido.dots, kaleido.lines_all, kaleido.lines);
-            break;
-        }
-        case 3: {
-            currentEffect = new Outwards(kaleido.lines);
-            break;
-        }
-        case 4: {
-            currentEffect = new Stars(kaleido.dots);
-            break;
-        }
-        }
+        int i = random(0, effects.size());
+        (effects[i])(this);
+    }
+
+    void sinesSlow()
+    {
+        Sines *sinesSlow = new Sines(kaleido.lines);
+        sinesSlow->minLength = 1.0f;
+        sinesSlow->maxLength = 5.0f;
+        sinesSlow->minStepVelocity = 1.0f;
+        sinesSlow->maxStepVelocity = 2.5f;
+        currentEffect = sinesSlow;
+    }
+
+    void sinesFast()
+    {
+        Sines *sinesFast = new Sines(kaleido.lines);
+        sinesFast->minLength = 5.0f;
+        sinesFast->maxLength = 15.0f;
+        sinesFast->minStepVelocity = 1.0f;
+        sinesFast->maxStepVelocity = 2.5f;
+        currentEffect = sinesFast;
+    }
+
+    void start()
+    {
+        currentEffect = new Start(kaleido.facets, kaleido.dots, kaleido.lines_all, kaleido.lines);
+    }
+
+    void outwards()
+    {
+        currentEffect = new Outwards(kaleido.lines);
+    }
+
+    void outwardsFacets()
+    {
+        currentEffect = new Outwards(kaleido.facets);
+    }
+
+    void stars()
+    {
+        currentEffect = new Stars(kaleido.dots);
+    }
+
+    void hopeFacets()
+    {
+        currentEffect = new Hope(kaleido.facets);
+    }
+
+    void hopeLines()
+    {
+        currentEffect = new Hope(kaleido.lines);
     }
 };
 
